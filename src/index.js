4e27c55e95899bc7fc87718ce1ebe560cd40f98b
Den <fabri.din@yandex.ru>
2020-07-14 18:00:11 +0500

    Добавит пропсы в компонент

--- src/index.js
+++ src/index.js
@@ -2,4 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 
-ReactDOM.render(<App />, document.querySelector(`#root`));
+const Settings = {
+  AMOUNT: 312
+};
+
+
+ReactDOM.render(<App placesAmount={Settings.AMOUNT} />, document.querySelector(`#root`));
