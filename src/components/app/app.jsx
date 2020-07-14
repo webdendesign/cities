c3fcdddb247d28c21354350ff19a992d942f1e9d
Den <fabri.din@yandex.ru>
2020-07-14 16:08:31 +0500

    Установит React и создаст первые компоненты

---
+++ src/components/app/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Main from '../main/main.jsx';
+
+const App = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const {placesAmount} = props;
+  return <Main placesAmount={placesAmount} />;
+};
+
+export default App;
