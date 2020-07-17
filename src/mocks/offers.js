f6bcb90a6882c6f461c557ea4ffedd97ea058d89
Den <fabri.din@yandex.ru>
2020-07-17 15:15:25 +0500

    Создаст моки для разработки

---
+++ src/mocks/offers.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    propertyName: `Beautiful luxurious apartment at great location`,
+    propertyType: `apartment`,
+    price: 120,
+    rating: 4,
+    isPremium: true,
+    picture: `apartment-01.jpg`,
+    id: uuidv4()
+  },
+  {
+    propertyName: `Wood and stone place`,
+    propertyType: `private room`,
+    price: 80,
+    rating: 4,
+    isPremium: false,
+    picture: `room.jpg`,
+    id: uuidv4()
+  },
+  {
+    propertyName: `Canal View Prinsengracht`,
+    propertyType: `apartment`,
+    price: 132,
+    rating: 4,
+    isPremium: false,
+    picture: `apartment-02.jpg`,
+    id: uuidv4()
+  },
+  {
+    propertyName: `Nice, cozy, warm big bed apartment`,
+    propertyType: `apartment`,
+    price: 180,
+    rating: 5,
+    isPremium: true,
+    picture: `apartment-03.jpg`,
+    id: uuidv4()
+  }
+];
